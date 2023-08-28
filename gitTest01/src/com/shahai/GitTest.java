81ce4dcf1c940fa9850d6bbb751fed10fc4cee3f
zi <2929522447@qq.com>
2023-08-28 11:51:19 +0800

    文件简化修改

--- gitTest01/src/com/shahai/GitTest.java
+++
@@ -1,9 +0,0 @@
-package com.shahai;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("这是第一行");
-        System.out.println("这是第二行");
-        System.out.println("这是第三行");
-    }
-}
